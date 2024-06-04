c639b9ee97599f82d44e88765c3ec30854252c5a
Clement Ojiguo <clementojigs@hotmail.com>
2024-06-03 17:59:21 -0700

    added listing details UI

---
+++ AirBnBswiftUI/Core/Components/ListingImagesCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImagesCarouselView.swift
+//  AirBnBswiftUI
+//
+//  Created by King OJ on 03/06/2024.
+//
+
+import SwiftUI
+
+struct ListingImagesCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImagesCarouselView()
+}
